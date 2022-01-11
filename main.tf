e21384deb2c1aed3b8e9f74219ee72b9d73adb90
Alex Weber <71449212+EyDabbleyu@users.noreply.github.com>
2022-01-11 12:47:03 +0100

    Versuch den Webserver zu deployen

--- main.tf
+++ main.tf
@@ -4,6 +4,16 @@ provider "aws" {
     secret_key = "GKrnfgcNSVHJ3dyo6sf2SMcTk3zPC6UyGjyj2mN9"
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]   
+        }
+    }
+
 resource "aws_instance" "example" {
     ami                     = "ami-0c55b159cbfafe1f0"
     instance_type           = "t2.micro"
@@ -16,15 +26,5 @@ resource "aws_instance" "example" {
                 EOF
     tags = {
         Name = "terraform-example"
-    }
-
-resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
-    ingress {
-        from_port   = 8080
-        to_port     = 8080
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]   
-        }
     }
 }
